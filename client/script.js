799c1008116f6538302244cba62c52bdf1808e8f
Harkins12345 <michaelhmeloy@gmail.com>
2022-03-30 12:07:49 +0100

    Added feeling lucky functionality

--- client/script.js
+++ client/script.js
@@ -1,17 +1,24 @@
-const button = document.getElementById("search-button")
+const form = document.getElementById("search-form")
 
-button.addEventListener("click", sendSearch)
+form.addEventListener("submit", sendSearch)
 
 //Send the search query
-function sendSearch() {
-    const params = new URLSearchParams ({
-        q: "coffee",
+function sendSearch(e) {
+    e.preventDefault();
+    const params = new URLSearchParams({
+        q: e.target.searchquery.value,
     })
 
     const url = `http://localhost:3000/search?${params.toString()}`
 
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+        if (e.submitter.name === "search") {
+            console.log(data)
+        } else {
+            window.location.href = data['organic_results'][0]['link']
+        }
+    })
     .catch(console.warn)
 }
--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ const api_key = "d500ef44ddda8ba101535c5264265a1f857a7e2408e7f6b7d9d71ecf6089f06
 
 app.get('/search', (req, res) => {
     const search_query = req.query
+    console.log(search_query.q)
     const params = new URLSearchParams ({
         q: `${search_query.q}`,
         location: "London",
@@ -21,6 +22,7 @@ app.get('/search', (req, res) => {
 
     const url = `https://serpapi.com/search.json?${params.toString()}`
 
+    //Uncomment to get live data
     /*fetch(url)
     .then(response => response.json())
     .then(data => res.json(data))
