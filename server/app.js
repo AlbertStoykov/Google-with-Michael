1bb8617ca57ac2ac5701e552e335918cce417bdb
Harkins12345 <michaelhmeloy@gmail.com>
2022-03-30 16:35:55 +0100

    Populating Search Results Functionality

--- server/app.js
+++ server/app.js
@@ -4,35 +4,36 @@ const cors = require('cors');
 const app = express();
 const dummy_data = require('./data')
 
+const api_key = "d500ef44ddda8ba101535c5264265a1f857a7e2408e7f6b7d9d71ecf6089f06d"
+
 app.use(cors());
 app.use(express.static('public'));
-
-const api_key = "d500ef44ddda8ba101535c5264265a1f857a7e2408e7f6b7d9d71ecf6089f06d"
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html')
 })
 
 app.get('/search', (req, res) => {
-    const search_query = req.query
-    console.log(search_query.q)
-    const params = new URLSearchParams ({
-        q: `${search_query.q}`,
+    res.sendFile(__dirname + '/public/search.html')
+})
+
+app.get('/requestResults', (req, res) => {
+    //Uncomment to get live data
+    /*
+    const params = new URLSearchParams({
+        q: req.query.q,
         location: "London",
         google_domain: "google.co.uk",
         safe: "active",
         engine: "google",
         api_key: api_key
     })
-
-    //Uncomment to get live data
-    //const url = `https://serpapi.com/search.json?${params.toString()}`
-    /*fetch(url)
+    const url = `https://serpapi.com/search.json?${params.toString()}`
+    fetch(url)
     .then(search_response => search_response.json())
     .then(data => res.json(data))
     .catch(console.warn)*/
-
-    res.sendFile(__dirname + '/public/search.html')
+    res.json(dummy_data)
 })
 
 module.exports = app
--- server/public/indexScript.js
+++ server/public/indexScript.js
@@ -1,5 +1,4 @@
 const form = document.getElementById("search-form")
-
 form.addEventListener("submit", sendSearch)
 
 //Send the search query
@@ -10,12 +9,21 @@ function sendSearch(e) {
             q: e.target.searchquery.value.trim(),
         })
     
-        const url = `http://localhost:3000/search?${params.toString()}`
+        const searchUrl = `http://localhost:3000/search?${params.toString()}`
 
         if (e.submitter.name === "search") {
-            window.location.href = url;
+            window.location.href = searchUrl;
         } else {
-            fetch(url)
+            const params = new URLSearchParams ({
+                q: `${e.target.searchquery.value.trim()}`,
+                location: "London",
+                google_domain: "google.co.uk",
+                safe: "active",
+                engine: "google",
+                api_key: api_key
+            })
+            const dataUrl = `http://localhost:3000/requestResults?${params.toString()}`
+            fetch(dataUrl)
             .then(res => res.json())
             .then(data => {window.location.href = data['organic_results'][0]['link'];})
             .catch(console.warn)
