6677421ec2b9040f65bb3d123ef94cb14980552a
Harkins12345 <michaelhmeloy@gmail.com>
2022-03-30 12:18:54 +0100

    Fixed blank search bug

--- client/script.js
+++ client/script.js
@@ -5,20 +5,22 @@ form.addEventListener("submit", sendSearch)
 //Send the search query
 function sendSearch(e) {
     e.preventDefault();
-    const params = new URLSearchParams({
-        q: e.target.searchquery.value,
-    })
-
-    const url = `http://localhost:3000/search?${params.toString()}`
-
-    fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        if (e.submitter.name === "search") {
-            console.log(data)
-        } else {
-            window.location.href = data['organic_results'][0]['link']
-        }
-    })
-    .catch(console.warn)
+    if (e.target.searchquery.value.trim()) {
+        const params = new URLSearchParams({
+            q: e.target.searchquery.value.trim(),
+        })
+    
+        const url = `http://localhost:3000/search?${params.toString()}`
+    
+        fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            if (e.submitter.name === "search") {
+                console.log(data)
+            } else {
+                window.location.href = data['organic_results'][0]['link']
+            }
+        })
+        .catch(console.warn)
+    }
 }
