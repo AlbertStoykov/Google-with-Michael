65069657e47b803faf14c0de5b05d6442d3c44b1
Harkins12345 <49042556+Harkins12345@users.noreply.github.com>
2022-03-31 03:47:10 +0100

    Update populateResults.js

--- server/public/populateResults.js
+++ server/public/populateResults.js
@@ -1,20 +1,26 @@
 function collectResultData() {
     const params = (new URL(document.location)).searchParams;
+    // API request to get search result data
     const dataUrl = `http://localhost:3000/requestResults?${params.toString()}`
     fetch(dataUrl)
     .then(res => res.json())
+    // Use it to populate the webpage
     .then(populateSearchResults)
     .catch(console.warn)
 }
 
 function populateSearchResults(data) {
+    // Time taken in seconds for search results and the amount of them
     document.getElementById("time-taken").innerHTML = `About ${data['search_information'].total_results.toLocaleString("en-US")} results (${data['search_information'].time_taken_displayed} seconds)` 
-
+    
+    // Normal Google search results with corrisponding links
     data['organic_results'].forEach(result => {
         document.getElementById(`result${result.position}-heading`).innerHTML = result.title;
         document.getElementById(`result${result.position}-heading`).href = result.link;
         document.getElementById(`result${result.position}-desc`).innerHTML = result.snippet;
     });
+    
+    // Hide the "People also ask" table unless it is included in the search results
     document.getElementById("question-table").style.display = "none"
     if (data['related_questions'] && data['related_questions'] !== undefined) {
         document.getElementById("question-table").style.display = "block";
@@ -34,4 +40,5 @@ function populateSearchResults(data) {
     }
 }
 
+//Run as soon as "http://localhost:3000/search?" is loaded
 collectResultData()
